297d2bf775df2abb5eaabd0ea685066a05b6080c
Emily Foley <foleyemilym@gmail.com>
2014-03-25 14:18:59 -0700

    updated bare bones view, routes

--- emidotly/app/controllers/urls_controller.rb
+++ emidotly/app/controllers/urls_controller.rb
@@ -1,6 +1,13 @@
+require 'faker'
+
 class UrlsController < ApplicationController
 
   def index
+  end
+
+  def new
+    @url = Url.create(long_url: params[:url],
+                      short_url: Faker::Lorem.characters(char_count=6))
   end
 
 end
--- emidotly/app/models/url.rb
+++ emidotly/app/models/url.rb
@@ -1,9 +1,6 @@
-class Urls < ActiveRecord::Base
+class Url < ActiveRecord::Base
+  attr_accessible :short_url, :long_url
   validates_presence_of :short_url
   validates_presence_of :long_url
-
-  def shorten_url(params)
-
-  end
 
 end
