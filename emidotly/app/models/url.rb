f8bf8a9da4ddac188e8348755fcc91f9eb831e9d
Emily Foley <foleyemilym@gmail.com>
2014-04-03 16:01:13 -0700

    added Rspec unit testing with shoulda matchers and Factory Girl

--- emidotly/app/models/url.rb
+++ emidotly/app/models/url.rb
@@ -2,5 +2,6 @@ class Url < ActiveRecord::Base
   attr_accessible :short_url, :long_url
   validates_presence_of :short_url
   validates_presence_of :long_url
+  validates_format_of :long_url, :with => URI::regexp(%w(http https))
 
 end
--- emidotly/config/environment.rb
+++ emidotly/config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Emidotly::Application.initialize!
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+end
