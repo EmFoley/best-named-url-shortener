54abe1266b318308e704fa6001fca9d4ddbcf15e
Emily Foley <foleyemilym@gmail.com>
2014-04-18 10:58:57 -0700

    fixed Rspec bugs

--- emidotly/config/environment.rb
+++ emidotly/config/environment.rb
@@ -1,9 +1,5 @@
-# Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Emidotly::Application.initialize!
 
-RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-end
