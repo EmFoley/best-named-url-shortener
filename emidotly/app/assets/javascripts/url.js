0054632e537100535f9e13356c6ff8598625fabf
Emily Foley <foleyemilym@gmail.com>
2014-06-18 16:09:42 -0700

    xhr responseText debugging

--- emidotly/app/assets/javascripts/url.js
+++ emidotly/app/assets/javascripts/url.js
@@ -1,15 +1,5 @@
-// var shortened = {
-//   bindEventListeners: function(){
-//     $('#long').on("ajax:success", $('#long').append('HELLO'))
-//   }
-// };
-
-// <%= render partial: "show", locals: { url: @url } %>
-
-
-
 $(document).ready(function() {
-  $("#long").on("ajax:success", function(e, data, status, xhr) {
+  return $("#long").on("ajax:success", function(e, data, status, xhr) {
     return $(".url_form").append(xhr.responseText);
   }).on("ajax:error", function(e, xhr, status, error) {
     return $(".url_form").append("<p>ERROR</p>");
